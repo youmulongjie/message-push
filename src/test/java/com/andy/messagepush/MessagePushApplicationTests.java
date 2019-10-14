fec87d968819307a637e38a8555e605d02d948bb
Andy <w_longjie@163.com>
2019-10-14 17:06:23 +0800

    初始上传

---
+++ src/test/java/com/andy/messagepush/MessagePushApplicationTests.java
@@ -0,0 +1,16 @@
+package com.andy.messagepush;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MessagePushApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
