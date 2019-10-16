f7efe084cb9f6d97c170b52dcc84f5470b9f12e7
Andy <w_longjie@163.com>
2019-10-16 16:45:33 +0800

    批量推（toList）封装方法及测试

---
+++ src/main/java/com/andy/messagepush/getui/push/GETUIPushToList.java
@@ -0,0 +1,338 @@
+package com.andy.messagepush.getui.push;
+
+import com.andy.messagepush.getui.GetUIAppInfo;
+import com.andy.messagepush.getui.param.GetUIStyleParam;
+import com.andy.messagepush.getui.template.GetUIPushTemplate;
+import com.andy.messagepush.getui.template.GetUIStyleTemplate;
+import com.gexin.rp.sdk.base.IPushResult;
+import com.gexin.rp.sdk.base.impl.ListMessage;
+import com.gexin.rp.sdk.base.impl.Target;
+import com.gexin.rp.sdk.http.IGtPush;
+import com.gexin.rp.sdk.template.AbstractTemplate;
+import com.gexin.rp.sdk.template.style.AbstractNotifyStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Description:个推平台 “批量推”，指向制定的一批用户推送消息。参考：http://docs.getui.com/getui/server/java/push/
+ * Author: Andy.wang
+ * Date: 2019/10/16 15:01
+ */
+@Component
+@Slf4j
+public class GETUIPushToList {
+    private static GETUIPushToList pushToList;
+
+    @PostConstruct
+    public void init() {
+        pushToList = this;
+        pushToList.appInfo = this.appInfo;
+    }
+
+    @Autowired
+    private GetUIAppInfo appInfo;
+
+    /**
+     * style0 （普通）样式 + NotificationTemplate 推送
+     *
+     * @param cidList             推送目标List
+     * @param title               标题
+     * @param text                内容
+     * @param transmissionContent 透析内容（可为空）
+     * @return
+     */
+    public final static String pushNotificationStyle0(List<String> cidList, String title, String text, String transmissionContent) {
+        GetUIStyleParam styleParam = new GetUIStyleParam();
+        styleParam.setTitle(title);
+        styleParam.setText(text);
+
+
+        AbstractNotifyStyle style = GetUIStyleTemplate.style0(styleParam);
+
+        try {
+            AbstractTemplate template = GetUIPushTemplate.notificationTemplate(style, transmissionContent, GetUIPushTemplate.TransmissionType.ONE);
+
+            return push(cidList, template);
+        } catch (Exception e) {
+            log.error("‘批量推’失败!", e);
+        }
+
+        return null;
+    }
+
+    /**
+     * style6-1 （图片）样式 + NotificationTemplate 推送
+     *
+     * @param cidList             推送目标List
+     * @param title               标题
+     * @param text                内容
+     * @param picUrl              通知栏图片URL地址
+     * @param transmissionContent 透析内容（可为空）
+     * @return
+     */
+    public final static String pushNotificationStyle6Pic(List<String> cidList, String title, String text, String picUrl, String transmissionContent) {
+        GetUIStyleParam styleParam = new GetUIStyleParam();
+        styleParam.setTitle(title);
+        styleParam.setText(text);
+
+        styleParam.setBigStyle6PicUrl(picUrl);
+
+
+        AbstractNotifyStyle style = GetUIStyleTemplate.style6Pic(styleParam);
+
+        try {
+            AbstractTemplate template = GetUIPushTemplate.notificationTemplate(style, transmissionContent, GetUIPushTemplate.TransmissionType.ONE);
+
+            return push(cidList, template);
+        } catch (Exception e) {
+            log.error("‘批量推’失败!", e);
+        }
+
+        return null;
+    }
+
+
+    /**
+     * style6-2 （长文本）样式 + NotificationTemplate 推送
+     *
+     * @param cidList             推送目标List
+     * @param title               标题
+     * @param text                内容
+     * @param bigText             长文本
+     * @param transmissionContent 透析内容（可为空）
+     * @return
+     */
+    public final static String pushNotificationStyle6Text(List<String> cidList, String title, String text, String bigText, String transmissionContent) {
+        GetUIStyleParam styleParam = new GetUIStyleParam();
+        styleParam.setTitle(title);
+        styleParam.setText(text);
+
+        styleParam.setBigStyle6Text(bigText);
+
+
+        AbstractNotifyStyle style = GetUIStyleTemplate.style6Text(styleParam);
+
+        try {
+            AbstractTemplate template = GetUIPushTemplate.notificationTemplate(style, transmissionContent, GetUIPushTemplate.TransmissionType.ONE);
+
+            return push(cidList, template);
+        } catch (Exception e) {
+            log.error("‘批量推’失败!", e);
+        }
+
+        return null;
+    }
+    //[NotificationTemplate] end
+
+
+    //[TransmissionTemplate] start
+
+    /**
+     * TransmissionContent 推送
+     *
+     * @param cidList             推送目标List
+     * @param transmissionContent
+     * @return
+     */
+    public final static String pushTransmission(List<String> cidList, String transmissionContent) {
+        try {
+            AbstractTemplate template = GetUIPushTemplate.transmissionTemplate(transmissionContent, GetUIPushTemplate.TransmissionType.ONE);
+
+            push(cidList, template);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    //[TransmissionTemplate] end
+
+    //[LinkTemplate] start
+
+    /**
+     * style0 （普通）样式 + LinkTemplate 推送
+     *
+     * @param cidList             推送目标List
+     * @param title   标题
+     * @param text    内容
+     * @param linkUrl 跳转链接地址
+     * @return
+     */
+    public final static String pushLinkStyle0(List<String> cidList, String title, String text, String linkUrl) {
+        GetUIStyleParam styleParam = new GetUIStyleParam();
+        styleParam.setTitle(title);
+        styleParam.setText(text);
+
+        AbstractNotifyStyle style = GetUIStyleTemplate.style0(styleParam);
+
+        try {
+            AbstractTemplate template = GetUIPushTemplate.linkTemplate(style, linkUrl);
+
+            push(cidList, template);
+        } catch (Exception e) {
+            log.error("‘批量推’失败!", e);
+        }
+
+        return null;
+    }
+
+    /**
+     * style6-1 （图片）样式 + LinkTemplate 推送
+     *
+     * @param cidList             推送目标List
+     * @param title   标题
+     * @param text    内容
+     * @param picUrl  通知栏图片URL地址
+     * @param linkUrl 跳转链接地址
+     * @return
+     */
+    public final static String pushLinkStyle6Pic(List<String> cidList, String title, String text, String picUrl, String linkUrl) {
+        GetUIStyleParam styleParam = new GetUIStyleParam();
+        styleParam.setTitle(title);
+        styleParam.setText(text);
+
+        styleParam.setBigStyle6PicUrl(picUrl);
+
+
+        AbstractNotifyStyle style = GetUIStyleTemplate.style6Pic(styleParam);
+
+        try {
+            AbstractTemplate template = GetUIPushTemplate.linkTemplate(style, linkUrl);
+
+            push(cidList, template);
+        } catch (Exception e) {
+            log.error("‘批量推’失败!", e);
+        }
+
+        return null;
+    }
+
+
+    /**
+     * style6-2 （长文本）样式 + LinkTemplate 推送
+     *
+     * @param cidList             推送目标List
+     * @param title   标题
+     * @param text    内容
+     * @param bigText 长文本
+     * @param linkUrl 跳转链接地址
+     * @return
+     */
+    public final static String pushLinkStyle6Text(List<String> cidList, String title, String text, String bigText, String linkUrl) {
+        GetUIStyleParam styleParam = new GetUIStyleParam();
+        styleParam.setTitle(title);
+        styleParam.setText(text);
+
+        styleParam.setBigStyle6Text(bigText);
+
+
+        AbstractNotifyStyle style = GetUIStyleTemplate.style6Text(styleParam);
+
+        try {
+            AbstractTemplate template = GetUIPushTemplate.linkTemplate(style, linkUrl);
+
+            push(cidList, template);
+        } catch (Exception e) {
+            log.error("‘批量推’失败!", e);
+        }
+
+        return null;
+    }
+    //[LinkTemplate] end
+
+
+    ///////////以上封装方法，便于调用（若不满足情况，可用下面方法自己组装，调用可参考测试类ToListPushTest.java）//////////////
+
+    /**
+     * “批量推” 消息
+     *
+     * @param cidList  目标集合
+     * @param template 推送模板
+     * @return
+     */
+    public final static String push(List<String> cidList, AbstractTemplate template) {
+        log.info("向用户【{}】‘批量推’一条消息.", cidList);
+        // 配置返回每个用户返回用户状态，可选
+        System.setProperty("gexin_pushList_needDetails", "true");
+
+        IGtPush push = new IGtPush(pushToList.appInfo.getAppKey(),
+                pushToList.appInfo.getMasterSecret());
+
+        // STEP1：创建消息公共体，获取taskId
+        String taskId = getContentId(push, template);
+
+        List<Target> targetList = new ArrayList<>(cidList.size());
+
+        // 循环添加 添加目标对象
+        cidList.forEach(cid -> {
+
+            Target target = new Target();
+            target.setAppId(pushToList.appInfo.getAppId());
+            target.setClientId(cid);
+
+            targetList.add(target);
+
+        });
+
+        // STEP2：调用【toList】执行推送接口执行下发
+        IPushResult ret = push.pushMessageToList(taskId, targetList);
+
+        String result = null;
+        if (ret != null) {
+            result = ret.getResponse().toString();
+            log.info("推送结果：{}", result);
+        } else {
+            log.info("服务器响应异常");
+        }
+
+        return result;
+    }
+
+    /**
+     * 获取 taskId
+     *
+     * @param push
+     * @param template
+     * @return
+     */
+    private static String getContentId(IGtPush push, AbstractTemplate template) {
+        ListMessage listMessage = listMessage(template);
+
+        String contentId = push.getContentId(listMessage);
+
+        log.info("获取 taskId = {}.", contentId);
+        return contentId;
+    }
+
+    /**
+     * 设置 批量推 消息类型 ListMessage
+     * <pre>
+     *     参数信息：
+     *      isOffline 	        boolean 	否 	false 	是否保持离线消息
+     *      offlineExpireTime 	long 	    否 	1小时 	过多久该消息离线失效（单位毫秒） 支持1-72小时*3600000毫秒
+     *      pushNetWorkType 	long 	    否 	0 	    推送网络要求
+     *                                                      0:联网方式不限;
+     *                                                      1:仅wifi;
+     *                                                      2:仅移动网络
+     *      data 	        ITemplate 	    是 	无 	    设置推送效果，点击查看详细说明
+     * </pre>
+     *
+     * @param template
+     * @return
+     */
+    private final static ListMessage listMessage(AbstractTemplate template) {
+        log.info("创建消息公共体 ListMessage.");
+
+        ListMessage message = new ListMessage();
+        message.setData(template);
+        message.setOffline(true);
+        message.setOfflineExpireTime(2 * 3600 * 1000);
+        message.setPushNetWorkType(0);
+        return message;
+    }
+}
---
+++ src/test/java/com/andy/messagepush/getui/ToListPushTest.java
@@ -0,0 +1,144 @@
+package com.andy.messagepush.getui;
+
+import com.andy.messagepush.getui.param.GetUIStyleParam;
+import com.andy.messagepush.getui.push.GETUIPushToList;
+import com.andy.messagepush.getui.template.GetUIPushTemplate;
+import com.andy.messagepush.getui.template.GetUIStyleTemplate;
+import com.gexin.rp.sdk.template.AbstractTemplate;
+import com.gexin.rp.sdk.template.style.AbstractNotifyStyle;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Description: 批量推（toList） 测试
+ * Author: Andy.wang
+ * Date: 2019/10/16 15:33
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToListPushTest {
+    private String cid = "a06ee9646c5dd70fb96ff989e5bc1839";
+    private String cid2 = "9c852a42e228ecb221c2710b6968f0d8";
+
+    // 测试 单推（style0 样式 + NotificationTemplate）
+    @Test
+    public void push() {
+        GetUIStyleParam styleParam = new GetUIStyleParam();
+        styleParam.setTitle("发工资了");
+        styleParam.setText("您的2019-10月份工资已到账，请注意查收");
+
+
+        AbstractNotifyStyle style = GetUIStyleTemplate.style0(styleParam);
+
+        String transmissionContent = "这是Andy.wang 用个推做的第三个demo（批量推：style0 样式 + NotificationTemplate）";
+
+        try {
+            AbstractTemplate template = GetUIPushTemplate.notificationTemplate(style, transmissionContent, GetUIPushTemplate.TransmissionType.ONE);
+
+            List<String> cidList = new ArrayList<>(2);
+            cidList.add(cid);
+            cidList.add(cid2);
+
+            GETUIPushToList.push(cidList, template);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /////////////////////////////以下是封装的方法，便于调用//////////////////////////////////
+
+    // 构造 消息接收目标 集合
+    private List<String> buildCidList() {
+        List<String> cidList = new ArrayList<>(2);
+        cidList.add(cid);
+        cidList.add(cid2);
+
+        return cidList;
+    }
+
+    // 测试 单推（style0 样式 + NotificationTemplate）,同 方法 toSingle()
+    @Test
+    public void pushNotificationStyle0() {
+        String title = "饭点到了";
+        String text = "韩梅梅你妈喊你回家吃中午饭了";
+
+        GETUIPushToList.pushNotificationStyle0(buildCidList(), title, text, null);
+    }
+
+    // 测试 单推（style6-1 样式 + NotificationTemplate）
+    @Test
+    public void pushNotificationStyle6Pic() {
+        String title = "开源中国";
+        String text = "我的开源中国账号头像是葫芦娃";
+        String picUrl = "https://oscimg.oschina.net/oscnet/up-c433d2b39ed5624fd813783da456026b.jpg";
+
+        GETUIPushToList.pushNotificationStyle6Pic(buildCidList(), title, text, picUrl, null);
+    }
+
+    // 测试 单推（style6-2 样式 + NotificationTemplate）
+    @Test
+    public void pushNotificationStyle6Text() {
+        String title = "积分落户";
+        String text = "2019北京积分落户分数线出炉了";
+        String bigText = "2019北京积分落户分数线什么时候才能出来?10月15日，" +
+                "北京市人力资源社会保障局发布消息，本市2019年积分落户申报审核工作已全部结束，" +
+                "自10月15日起进入公示及落户办理阶段。" +
+                "届时，今年申报积分落户的106403名申请人可登录积分落户在线申报系统查看本人年度积分和排名";
+
+        GETUIPushToList.pushNotificationStyle6Text(buildCidList(), title, text, bigText, null);
+    }
+
+    // 测试 单推（TransmissionTemplate），同方法 toSingle2()
+    @Test
+    public void pushTransmission() {
+        String transmissionContent = "2019北京积分落户分数线什么时候才能出来?10月15日，" +
+                "北京市人力资源社会保障局发布消息，本市2019年积分落户申报审核工作已全部结束，" +
+                "自10月15日起进入公示及落户办理阶段。" +
+                "届时，今年申报积分落户的106403名申请人可登录积分落户在线申报系统查看本人年度积分和排名";
+
+        GETUIPushToList.pushTransmission(buildCidList(), transmissionContent);
+    }
+
+    // 测试 单推（style0 样式 + NotificationTemplate）,同 方法 toSingle3()
+    @Test
+    public void pushLinkStyle0() {
+        String title = "github采访";
+        String text = "欢迎来到我的github主页，喜欢的话留下star，谢谢";
+
+        String linkUrl = "https://github.com/youmulongjie";
+
+        GETUIPushToList.pushLinkStyle0(buildCidList(), title, text, linkUrl);
+    }
+
+    // 测试 单推（style6-1 样式 + NotificationTemplate）
+    @Test
+    public void pushLinkStyle6Pic() {
+        String title = "开源中国";
+        String text = "hello，我是葫芦娃，欢迎来到我的开源中国账号";
+        String picUrl = "https://oscimg.oschina.net/oscnet/up-c433d2b39ed5624fd813783da456026b.jpg";
+
+        String linkUrl = "https://my.oschina.net/andy1989";
+
+        GETUIPushToList.pushLinkStyle6Pic(buildCidList(), title, text, picUrl, linkUrl);
+    }
+
+    // 测试 单推（style6-2 样式 + NotificationTemplate）
+    @Test
+    public void pushLinkStyle6Text() {
+        String title = "积分落户";
+        String text = "关于公布2019年积分落户公示名单的通告";
+        String bigText = "根据《北京市积分落户管理办法（试行）》（京政办发〔2016〕39号）、《北京市积分落户操作管理细则（试行）》" +
+                "（京人社调发〔2018〕64号）有关规定，经北京市人民政府批准，现将2019年积分落户人员名单公示如下（具体名单见附件），" +
+                "接受社会监督";
+
+        String linkUrl = "http://rsj.beijing.gov.cn/jflh/jf_zcwj/jf_gg/201910/t20191015_85290.html";
+
+        GETUIPushToList.pushLinkStyle6Text(buildCidList(), title, text, bigText, linkUrl);
+    }
+}
